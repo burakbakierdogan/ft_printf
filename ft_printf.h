fbe749861f65e3929671be91337edf1537f17866
burak baki erdogan <bbe2007@outlook.com>
2022-07-23 16:12:11 +0300

    done

---
+++ ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 15:43:31 by berdogan          #+#    #+#             */
+/*   Updated: 2022/07/23 15:46:59 by berdogan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int	ft_printf(cost char *src, ...);
+
+
+
+#endif

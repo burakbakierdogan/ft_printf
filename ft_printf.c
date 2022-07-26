b6694400a3dbdc4b370d2e0a6ee947d568157742
burak baki erdogan <bbe2007@outlook.com>
2022-07-26 07:43:49 +0300

    working on

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,22 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 15:32:52 by berdogan          #+#    #+#             */
-/*   Updated: 2022/07/26 06:13:32 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/07/26 06:17:55 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 
-static	int	ft_write(char *src)
+static	int	ft_write(char *src, va_list list)
 {
-	
+	int	ret;
+
+	ret = 0;
+	if (src[1] == 'c')
+		ret = ft_putchar(va_arg(list, int));
+	else if (src[1] == 'd')
+		ret = 
 }
 
 int	ft_printf(const char *src, ...)
@@ -31,7 +37,7 @@ int	ft_printf(const char *src, ...)
 	{
 		if (src[index] == '%')
 		{
-			ret += ft_write(src[index++]);
+			ret += ft_write(src[index++], list);
 			index++;
 		}
 		else
